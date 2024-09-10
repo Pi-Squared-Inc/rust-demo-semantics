283710d27267462509912ed5254aabb6fab702f5
acassimiro <cassimiroaellison@gmail.com>
2024-09-10 18:24:29 -0300

    Adding initial tests

---
+++ tests/execution/if-expressions.rs
@@ -0,0 +1,29 @@
+#![no_std]
+
+#[allow(unused_imports)]
+use multiversx_sc::imports::*;
+
+#[multiversx_sc::contract]
+pub trait IfExpressions {
+    #[init]
+    fn init(&self) {
+    }
+
+    #[upgrade]
+    fn upgrade(&self) {}
+    
+    fn if_expression(&self) -> bool { 
+        let x = 80_u64;
+        let mut b: bool = false;
+    
+        if (x > 80_u64){
+            b = true;
+        } else {
+            b = false;
+        };
+        
+        b
+
+    }
+
+}
