c7e509ea679808813891d51823f1ce1efcb64209
ACassimiro <cassimiroaellison@gmail.com>
2024-09-26 22:27:53 -0300

    Enabling assignments on tuples with let expressions (#107)
    * Enabling assignments on tuples with let expressions
    
    * Addressing comments

--- tests/execution/let-expression.rs
+++ tests/execution/let-expression.rs
@@ -4,7 +4,7 @@
 use multiversx_sc::imports::*;
 
 #[multiversx_sc::contract]
-pub trait LetFinal {
+pub trait Let {
     #[init]
     fn init(&self) {
     }
@@ -14,6 +14,17 @@ pub trait LetFinal {
 
     fn let_final(&self) -> u64 {
         let x = 100_u64;
+        x
+    }
+
+    fn let_mut(&self) -> u64 {
+        let mut x = 99_u64;
+        x = 100_u64;
+        x
+    }
+
+    fn let_tuple(&self) -> u64 {
+        let (x, y, z, w,) = (100_u64, 99_u64, 98_u64, 97_u64);
         x
     }
 }
