3ff8940415402c414b15ab6ff8668f2e94221f44
Virgil <virgil.serbanuta@runtimeverification.com>
2024-10-07 18:36:31 +0300

    Tests for top level fn without args

--- tests/execution/top_level_fn.rs
+++ tests/execution/top_level_fn.rs
@@ -11,12 +11,20 @@ pub trait TopLevelFn {
 
     #[upgrade]
     fn upgrade(&self) {}
-    
+
     fn call_top_level(&self, v: u64) -> u64 { 
       ::top_level_fn::top_level(v)
+    }
+
+    fn call_top_level_no_arg(&self) -> u64 { 
+      ::top_level_fn::top_level_no_arg()
     }
 }
 
 fn top_level(v: u64) -> u64 {
   v + 3_u64
+}
+
+fn top_level_no_arg() -> u64 {
+  3_u64
 }
