90be9bb13efe1c2dda707005fa39be68e1fa1e84
acassimiro <cassimiroaellison@gmail.com>
2024-10-08 00:46:09 -0300

    Adding tests

--- tests/execution/structs.rs
+++ tests/execution/structs.rs
@@ -4,6 +4,7 @@
 use multiversx_sc::imports::*;
 
 struct Pair{i: u32, j: u32 }
+struct Triple{i: u32, j: u32, k: u32 }
 
 #[multiversx_sc::contract]
 pub trait StructFile {
@@ -14,5 +15,22 @@ pub trait StructFile {
     #[upgrade]
     fn upgrade(&self) {}
 
-    fn empty(&self) {}
+    fn struct_with_var_assignment(&self) -> u32 {
+        let _pair = ::structs::Pair {
+            i: 1_u32,
+            j: 2_u32
+        };
+        let x = _pair.j;
+        x
+    }
+
+    fn struct_without_var_assignment(&self) -> u32 {
+        let _triple = ::structs::Triple {
+            1_u32,
+            2_u32,
+            3_u32
+        };
+        let x = _triple.k;
+        x
+    }
 }
