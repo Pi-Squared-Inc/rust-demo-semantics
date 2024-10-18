47d87d51c2568e1fb7a47b63e91d744f552ff657
Virgil <virgil.serbanuta@runtimeverification.com>
2024-10-18 14:43:46 +0300

    Use the new int types for erc-20

--- tests/ukm-with-contract/erc_20_token.rs
+++ tests/ukm-with-contract/erc_20_token.rs
@@ -32,7 +32,7 @@ Observations:
 pub trait Erc20Token {
     // #[view(totalSupply)]
     #[storage_mapper("total_supply")]
-    fn s_total_supply(&self) -> ::single_value_mapper::SingleValueMapper<u64>;
+    fn s_total_supply(&self) -> ::single_value_mapper::SingleValueMapper<u256>;
 
     // #[view(getName)]
     #[storage_mapper("name")]
@@ -44,21 +44,21 @@ pub trait Erc20Token {
 
     // #[view(getBalances)]
     #[storage_mapper("balances")]
-    fn s_balances(&self, address: u64) -> ::single_value_mapper::SingleValueMapper<u64>;
+    fn s_balances(&self, address: u160) -> ::single_value_mapper::SingleValueMapper<u256>;
 
     // #[view(getAllowances)]
     #[storage_mapper("balances")]
-    fn s_allowances(&self, account: u64, spender: u64) -> ::single_value_mapper::SingleValueMapper<u64>;
+    fn s_allowances(&self, account: u160, spender: u160) -> ::single_value_mapper::SingleValueMapper<u256>;
 
     #[event("Transfer")]
-    fn transfer_event(&self, #[indexed] from: u64, #[indexed] to: u64, value: u64);
+    fn transfer_event(&self, #[indexed] from: u160, #[indexed] to: u160, value: u256);
 
     #[event("Approval")]
-    fn approval_event(&self, #[indexed] owner: u64, #[indexed] spender: u64, value: u64);
+    fn approval_event(&self, #[indexed] owner: u160, #[indexed] spender: u160, value: u256);
 
 
     #[init]
-    fn init(&self, /*name: &ManagedBuffer, symbol: &ManagedBuffer, */init_supply: u64) {
+    fn init(&self, /*name: &ManagedBuffer, symbol: &ManagedBuffer, */init_supply: u256) {
         // self.s_name().set_if_empty(name);
         // self.s_symbol().set_if_empty(symbol);
         self._mint(::ukm::Caller(), init_supply);
@@ -72,6 +72,11 @@ pub trait Erc20Token {
         18
     }
 
+    #[view(totalSupply)]
+    fn total_supply(&self) -> u256 {
+        self.s_total_supply().get()
+    }
+
     // #[view(name)]
     // fn name(&self) -> ManagedBuffer {
     //     self.s_name().get()
@@ -83,45 +88,45 @@ pub trait Erc20Token {
     // }
 
     #[view(balanceOf)]
-    fn balance_of(&self, account: u64) -> u64 {
+    fn balance_of(&self, account: u160) -> u256 {
         self.s_balances(account).get()
     }
 
     #[endpoint(transfer)]
-    fn transfer(&self, to: u64, value: u64) -> bool {
+    fn transfer(&self, to: u160, value: u256) -> bool {
         let owner = ::ukm::Caller();
         self._transfer(&owner, to, &value);
         true
     }
 
     #[view(allowance)]
-    fn allowance(&self, owner: u64, spender: u64) -> u64 {
+    fn allowance(&self, owner: u160, spender: u160) -> u256 {
         self.s_allowances(owner, spender).get()
     }
 
     #[endpoint(approve)]
-    fn approve(&self, spender: u64, value: u64) -> bool {
+    fn approve(&self, spender: u160, value: u256) -> bool {
         let owner = ::ukm::Caller();
         self._approve(&owner, spender, value, true);
         true
     }
 
     #[endpoint(transferFrom)]
-    fn transfer_from(&self, from: u64, to: u64, value: u64) -> bool {
+    fn transfer_from(&self, from: u160, to: u160, value: u256) -> bool {
         let spender = ::ukm::Caller();
         self._spend_allowance(from, &spender, value);
         self._transfer(from, to, value);
         true
     }
 
-    fn _transfer(&self, from: u64, to: u64, value: u64) {
+    fn _transfer(&self, from: u160, to: u160, value: u256) {
         ::helpers::require(!::address::is_zero(from), "Invalid sender");
         ::helpers::require(!::address::is_zero(to), "Invalid receiver");
         self._update(from, to, value);
         self.transfer_event(from, to, value);
     }
 
-    fn _update(&self, from: u64, to: u64, value: u64) {
+    fn _update(&self, from: u160, to: u160, value: u256) {
         if ::address::is_zero(from) {
             self.s_total_supply().set(self.s_total_supply().get() + value);
         } else {
@@ -137,12 +142,12 @@ pub trait Erc20Token {
         }
     }
 
-    fn _mint(&self, account: u64, value: u64) {
+    fn _mint(&self, account: u160, value: u256) {
         ::helpers::require(!::address::is_zero(account), "Zero address");
-        self._update(0_u64, account, value);
+        self._update(0_u160, account, value);
     }
 
-    fn _approve(&self, owner: u64, spender: u64, value: u64, emit_event: bool) {
+    fn _approve(&self, owner: u160, spender: u160, value: u256, emit_event: bool) {
         ::helpers::require(!::address::is_zero(owner), "Invalid approver");
         ::helpers::require(!::address::is_zero(spender), "Invalid spender");
         self.s_allowances(owner, spender).set(value);
@@ -151,7 +156,7 @@ pub trait Erc20Token {
         }
     }
 
-    fn _spend_allowance(&self, owner: u64, spender: u64, value: u64) {
+    fn _spend_allowance(&self, owner: u160, spender: u160, value: u256) {
         let current_allowance = self.allowance(owner, spender);
         ::helpers::require(value <= &current_allowance, "Insuficient allowance");
         self._approve(owner, spender, &(current_allowance - value), false);
