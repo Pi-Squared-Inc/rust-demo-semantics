8fd9dd0c51a89b6cd37479464b9b7ba945de4533
Virgil <25692529+virgil-serbanuta@users.noreply.github.com>
2024-10-22 19:05:39 +0300

    Use the new int types for erc-20 (#165)

--- tests/ukm-contracts/test_helpers.rs
+++ tests/ukm-contracts/test_helpers.rs
@@ -6,3 +6,11 @@ fn decode_single_u64(bytes_id: u64) -> u64 {
   };
   value
 }
+
+fn decode_single_u256(bytes_id: u64) -> u256 {
+  let (remaining_id, value) = :: bytes_hooks :: decode_u256(bytes_id);
+  if :: bytes_hooks :: length(remaining_id) > 0_u32 {
+      fail();
+  };
+  value
+}
