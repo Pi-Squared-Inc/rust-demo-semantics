043f9dc2f29f23abdbf75b1735728408fe856f15
Virgil <25692529+virgil-serbanuta@users.noreply.github.com>
2024-10-25 23:58:58 +0300

    Update the readme file and some build scripts (#175)

--- compilation/prepare-contract.sh
+++ compilation/prepare-contract.sh
@@ -11,53 +11,7 @@ TEMP_DIR=tmp
 
 mkdir -p $TEMP_DIR
 
-echo "<(<" > $TEMP_DIR/input.tmp
-echo "::address" >> $TEMP_DIR/input.tmp
-echo "<|>" >> $TEMP_DIR/input.tmp
-cat $ULM_CONTRACTS_TESTING_INPUT_DIR/address.rs >> $TEMP_DIR/input.tmp
-echo ">)>" >> $TEMP_DIR/input.tmp
-
-echo "<(<" >> $TEMP_DIR/input.tmp
-echo "::bytes_hooks" >> $TEMP_DIR/input.tmp
-echo "<|>" >> $TEMP_DIR/input.tmp
-cat $ULM_CONTRACTS_TESTING_INPUT_DIR/bytes_hooks.rs >> $TEMP_DIR/input.tmp
-echo ">)>" >> $TEMP_DIR/input.tmp
-
-echo "<(<" >> $TEMP_DIR/input.tmp
-echo "::test_helpers" >> $TEMP_DIR/input.tmp
-echo "<|>" >> $TEMP_DIR/input.tmp
-cat $ULM_CONTRACTS_TESTING_INPUT_DIR/test_helpers.rs >> $TEMP_DIR/input.tmp
-echo ">)>" >> $TEMP_DIR/input.tmp
-
-echo "<(<" >> $TEMP_DIR/input.tmp
-echo "::helpers" >> $TEMP_DIR/input.tmp
-echo "<|>" >> $TEMP_DIR/input.tmp
-cat $ULM_CONTRACTS_TESTING_INPUT_DIR/helpers.rs >> $TEMP_DIR/input.tmp
-echo ">)>" >> $TEMP_DIR/input.tmp
-
-echo "<(<" >> $TEMP_DIR/input.tmp
-echo "::state_hooks" >> $TEMP_DIR/input.tmp
-echo "<|>" >> $TEMP_DIR/input.tmp
-cat $ULM_CONTRACTS_TESTING_INPUT_DIR/state_hooks.rs >> $TEMP_DIR/input.tmp
-echo ">)>" >> $TEMP_DIR/input.tmp
-
-echo "<(<" >> $TEMP_DIR/input.tmp
-echo "::single_value_mapper" >> $TEMP_DIR/input.tmp
-echo "<|>" >> $TEMP_DIR/input.tmp
-cat $ULM_CONTRACTS_TESTING_INPUT_DIR/single_value_mapper.rs >> $TEMP_DIR/input.tmp
-echo ">)>" >> $TEMP_DIR/input.tmp
-
-echo "<(<" >> $TEMP_DIR/input.tmp
-echo "::ulm" >> $TEMP_DIR/input.tmp
-echo "<|>" >> $TEMP_DIR/input.tmp
-cat $ULM_CONTRACTS_TESTING_INPUT_DIR/ulm.rs >> $TEMP_DIR/input.tmp
-echo ">)>" >> $TEMP_DIR/input.tmp
-
-echo "<(<" >> $TEMP_DIR/input.tmp
-echo "$1" | sed 's%^.*/%%' | sed 's/\..*//' | sed 's/^/::/' >> $TEMP_DIR/input.tmp
-echo "<|>" >> $TEMP_DIR/input.tmp
-cat "$1" >> $TEMP_DIR/input.tmp
-echo ">)>" >> $TEMP_DIR/input.tmp
+compilation/prepare_rust_bundle.sh $1 $TEMP_DIR/input.tmp
 
 krun \
   $TEMP_DIR/input.tmp \
@@ -66,5 +20,3 @@ krun \
   --output kore \
   --output-file $TEMP_DIR/output.kore \
 
-echo "not finished, must extract the bytes from the result: $TEMP_DIR/output.kore"
-false
